b48c84fe9c31b4455b31fb55eb0fc023fababf56
Sumtudou <1163015195@qq.com>
2020-08-17 20:49:51 +0800

    [merge][Jay] feature:modify  test2 again

--- src/main/Test2.java
+++ src/main/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("I am test2");
         System.out.println("I modify test2");
+        System.out.println("I am Jay , I modify test2 again");
     }
 }
